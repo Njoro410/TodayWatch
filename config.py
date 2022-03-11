1fe3aa220df1fc6289b66295501d0d0a05666512
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-11 18:01:09 +0300

    added a configuration file

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+
+class Config:
+
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+
+
+class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+
+class TestConfig(Config):
+
+
+    pass
+
+
+class DevConfig(Config):
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+
+}
