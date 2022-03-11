4803817017ca71c94cdc41adad725ef32d9360f1
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-11 18:03:34 +0300

    added class models for movie and trailer

---
+++ app/models.py
@@ -0,0 +1,31 @@
+
+import string
+
+
+class Movie:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self, id, title, overview, poster, backdrop, vote_average, vote_count, tagline, genres, date, language, prod, imdb_id, runtime):
+        self.id = id
+        self.title = title
+        self.overview = overview
+        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
+        self.backdrop = "https://image.tmdb.org/t/p/w500/" + (backdrop)
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+        self.tagline = tagline
+        self.genres = genres
+        self.date = date
+        self.language = language
+        self.prod = prod
+        self.imdb_id = imdb_id
+        self.runtime = runtime
+
+class Trailer:
+    def __init__(self,id,name,key,type):
+        self.id = id
+        self.name = name
+        self.key = key
+        self.type = type
