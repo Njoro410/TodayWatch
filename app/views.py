2eb7f15bc409f5164f4e086ee95f06c336a60c7d
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-11 18:57:11 +0300

    added a search movie function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 
-from app.request import get_movie, get_movies, get_trailer
+from app.request import get_movie, get_movies, get_trailer, search_movie
 
 views = Blueprint('views', __name__)
 
@@ -17,10 +17,10 @@ def index():
     # print(popular_movies)
     title = 'Home - Welcome to The best Movie Review Website Online'
     search_movie = request.args.get('movie_query')
-    # if search_movie:
-    #     return redirect(url_for('.search', movie_name=search_movie))
-    # else:
-    return render_template('index.html', title=title, popular=popular_movies, upcoming=upcoming_movie, now_showing=now_showing_movie)
+    if search_movie:
+        return redirect(url_for('.search', movie_name=search_movie))
+    else:
+        return render_template('index.html', title=title, popular=popular_movies, upcoming=upcoming_movie, now_showing=now_showing_movie)
 
 
 @views.route('/movie/<string:id>')
@@ -35,19 +35,17 @@ def movie(id):
     hours = time // 60
     minutes = time % 60
     time_string = "{}hrs:{}mins".format(hours, minutes)
-    
-
 
     return render_template('movie.html', title=title, movie=movie, time=time_string, trailers=trailer)
 
 
-# @views.route('/search/<movie_name>')
-# def search(movie_name):
-#     '''
-#     View function to display the search results
-#     '''
-#     movie_name_list = movie_name.split(" ")
-#     movie_name_format = "+".join(movie_name_list)
-#     searched_movies = search_movie(movie_name_format)
-#     title = f'search results for {movie_name}'
-#     return render_template('search.html', movies=searched_movies)
+@views.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display the search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html', movies=searched_movies)
